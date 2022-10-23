14ba5d6a23748196ff8c05936ab6120931219bc4
rrakib2 <rakib.rupom007@gmail.com>
2022-10-23 23:05:39 +0600

    loose vs string type checking

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,19 @@
+let first = 2;
+let second = "2";
+if (first === second) {
+    console.log("condition true");
+} else {
+    console.log("condition false");
+}
+
+/**
+ * in this case js try to convert one variable to another. if possible then it prints true.
+ * otherwise it prints false.
+ */
+let f = 2;
+let g = true;
+if (f === g) {
+    console.log("condition true");
+} else {
+    console.log("condition false");
+}
